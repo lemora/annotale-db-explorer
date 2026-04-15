294028f8fe1c8e7fc81fb140f65c73857f54f42d
lemora <12768489+lemora@users.noreply.github.com>
2026-04-15 13:34:34 +0200

    Home page: add link to GitHub repo

--- pages/00_Home.py
+++ pages/00_Home.py
@@ -3,13 +3,25 @@ import streamlit as st
 from utils.db import load_families, load_strains, load_tales
 from utils.page import init_page
 
+REPO_URL = "https://github.com/lemora/annotale-db-explorer"
+GITHUB_ICON_URL = (
+    "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+)
+HOME_CAPTION_HTML = (
+    '<p style="font-size:0.875rem;opacity:0.8;margin:0 0 1rem 0;">'
+    'Interactive explorer for the '
+    '<a href="https://github.com/jstacs/annotale" target="_blank">AnnoTALE</a> '
+    'SQLite database. '
+    f'<a href="{REPO_URL}" target="_blank" rel="noopener noreferrer" '
+    'aria-label="GitHub repository" title="GitHub repository" '
+    'style="display:inline-block;vertical-align:middle;margin-left:0.4rem;">'
+    f'<img src="{GITHUB_ICON_URL}" width="18" style="vertical-align:middle;"></a></p>'
+)
+
 init_page("Home", "Home", require_db=False)
 st.image("img/AnnoTALE_transp.png", width=160)
 st.title("AnnoTALE DB Explorer")
-st.caption(
-    "Interactive explorer for the "
-    "[AnnoTALE](https://github.com/jstacs/annotale) SQLite database."
-)
+st.markdown(HOME_CAPTION_HTML, unsafe_allow_html=True)
 st.markdown(
     """
     Use the sidebar to move through database overview, TALE distributions,
